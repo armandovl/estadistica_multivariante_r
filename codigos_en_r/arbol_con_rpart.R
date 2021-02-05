2500e8716fe5610890a1ca0a6b032158eb9b5b5d
armandovl <valdes.gam@gmail.com>
2021-02-05 02:25:46 -0600

    reacomodo de las lineas

--- codigos_en_r/arbol_con_rpart.R
+++ codigos_en_r/arbol_con_rpart.R
@@ -3,13 +3,10 @@
 
 #biblioteca para leer archivos de excel
 library(readxl)
-
 #bibliotecas para hacer el módelo
 library(rpart)
-
 #bibliotecas para graficar el módelo
 library(rpart.plot)
-
 #biblioteca para ver la importancia
 library(caret)
 
@@ -19,9 +16,7 @@ library(caret)
 # install.packages("rpart.plot")
 # install.packages("caret")
 
-
 #......... Cargar, inspeccionar y depurar datos...........#
-
 # cargamos el data set
 Datos1<-read_excel("bases_de_datos/datos_covid_30_mayo_2020.xlsx")
 
@@ -29,9 +24,7 @@ Datos1<-read_excel("bases_de_datos/datos_covid_30_mayo_2020.xlsx")
 miURL<-"https://raw.githubusercontent.com/armandovl/estadistica_multivariante_r/main/bases_de_datos/datos_covid_30_mayo_2020.csv"
 Datos1<-read.csv(url(miURL))
 
-
 str(Datos1) #estructura de los datos
-
 View(head(Datos1,10)) #ver primeros registros
 
 #eliminar columnas
@@ -39,19 +32,15 @@ Datos1$MUNICIPIO<-NULL
 Datos1$INTUBADO<-NULL
 Datos1$CUIDADOS_INTENSIVOS<-NULL
 
-
 #......... Se crea el modelo...........#
-
 #generando el modelo
 modelo1<-rpart(ESTIMADOS_SINTOMAS~.,
                method="class",
                data=Datos1)
 
-
 #ver importancia de las variables
 importanciaV<-varImp(modelo1)
 print(importanciaV)
-
 #vemos el resultado
 rpart.plot(modelo1,type=4)
 
