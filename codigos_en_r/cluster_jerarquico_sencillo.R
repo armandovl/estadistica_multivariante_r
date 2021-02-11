7e1aacfda5797a9e3f6dae2167593c34f3cf20b8
armandovl <valdes.gam@gmail.com>
2021-02-10 21:37:04 -0600

    listo, solo falta investigar el mètodo ward D2

--- codigos_en_r/cluster_jerarquico_sencillo.R
+++ codigos_en_r/cluster_jerarquico_sencillo.R
@@ -1,12 +1,23 @@
 
-#agregamos la base de datos
-datos<-read.csv("bases_de_datos/ejemplo_estudiantes.csv", 
+
+# #agregamos la base de datos
+# datos<-read.csv("bases_de_datos/ejemplo_estudiantes.csv", 
+#                 #en esta ìneale agregamos los nombres
+#                 row.names = 1,
+#                 # estàs lìneas no importan mucho
+#                 header=TRUE, #encabezados primera fila
+#                 sep=",", #separador por comas
+#                 dec=".") #los decimales con un punto
+
+miUrl<-"https://raw.githubusercontent.com/armandovl/estadistica_multivariante_r/main/bases_de_datos/ejemplo_estudiantes.csv"
+datos<-read.csv(url(miUrl),
                 #en esta ìneale agregamos los nombres
                 row.names = 1,
                 # estàs lìneas no importan mucho
                 header=TRUE, #encabezados primera fila
                 sep=",", #separador por comas
                 dec=".") #los decimales con un punto
+
 
 #creamos el modelo
 modelo<-hclust(dist(datos), method = "ward.D2")
